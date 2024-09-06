f876b13ce6b8194abd020aaf653de8a3a887af2c
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-09-06 09:11:44 +0545

    feat: add tab

--- src/app/tab-three/layout.tsx
+++ src/app/tab-three/layout.tsx
@@ -1,66 +1,10 @@
-"use client";
-import React, { useState, useEffect, useCallback } from "react";
-// import Link from "next/link";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { TabThreeNav } from "@/components";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const [activeTab, setActiveTab] = useState("1");
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString());
-      params.set(name, value);
-
-      return params.toString();
-    },
-    [searchParams]
-  );
-
-  useEffect(() => {
-    const tab = searchParams.get("tab") || "1";
-    setActiveTab(tab);
-  }, [searchParams]);
-
-  const tabs = [
-    { id: "1", label: "Tab 1" },
-    { id: "2", label: "Tab 2" },
-    { id: "3", label: "Tab 3" },
-  ];
-
   return (
     <div>
-      <nav role="tablist">
-        {tabs.map((tab) => (
-          <button
-            key={tab.id}
-            role="tab"
-            aria-selected={activeTab === tab.id}
-            aria-controls={`panel-${tab.id}`}
-            id={`tab-${tab.id}`}
-            tabIndex={activeTab === tab.id ? 0 : -1}
-            onClick={() => {
-              router.push(pathname + "?" + createQueryString("tab", tab.id));
-            }}
-          >
-            {/* <Link href={`?tab=${tab.id}`}>{tab.label}</Link> */}
-
-            {/* <Link
-              href={
-                // <pathname>?sort=desc
-                pathname + "?" + createQueryString("tab", tab.id)
-              }
-            >
-              {tab.label}
-            </Link> */}
-
-            {tab.label}
-          </button>
-        ))}
-      </nav>
+      <TabThreeNav />
       {children}
     </div>
   );
